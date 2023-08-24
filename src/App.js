a7f9fc016d1ff9e1c990d6d9acd1b911c65fa674
Shivakumara Bannimatti <shivaslb72@gmail.com>
2023-08-24 12:27:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Shiva's Portfolio!</h1>
+      <p>This is a simple React app created using Parcel.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
